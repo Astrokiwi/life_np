f3b87c2a470574ba93b58e0a899302ac73c46a86
David J Williamson <davidjwilliamson@Davids-MacBook-Pro.local>
2020-05-01 13:23:50 +0100

    faster image dumping and neighbour counting, uses ffmpeg to make mp4

--- life_np.py
+++ life_np.py
@@ -1,20 +1,38 @@
 import numpy as np
-import scipy.signal
-import matplotlib.pyplot as plt
+import subprocess
+import matplotlib.pyplot # required for PIL images, apparently?
+import PIL
 
-L = 256
-n_iterations = 100
 
-initial_probability = .5
-neighbour_template = np.ones((3,3),dtype=np.int)
-neighbour_template[1,1]=0
+def count_neighbours_roll(bool_map): # about 6x faster than scipy convolve2d
+    life_map = bool_map.astype(np.int)
+    neighbours = np.roll(life_map,1,axis=0)
+    neighbours += np.roll(life_map,-1,axis=0)
+    neighbours += np.roll(life_map,1,axis=1)
+    neighbours += np.roll(life_map,-1,axis=1)
+    neighbours += np.roll(life_map,(-1,-1),axis=(0,1))
+    neighbours += np.roll(life_map,(1,-1),axis=(0,1))
+    neighbours += np.roll(life_map,(-1,1),axis=(0,1))
+    neighbours += np.roll(life_map,(1,1),axis=(0,1))
+    return neighbours
 
-life_map = np.random.choice(a=[False, True], size=(L, L), p=[1-initial_probability,initial_probability])
+def game_of_life():
+    L = 1024
+    n_iterations = 100
 
-for iplot in range(n_iterations):
-    print(iplot)
-    plt.clf()
-    plt.imshow(life_map)
-    plt.savefig(f"pics/{iplot:06}.png",dpi=250)
-    neighbour_map = scipy.signal.convolve2d(life_map, neighbour_template, mode='same',boundary='wrap')
-    life_map = neighbour_map==3 | (neighbour_map==2 & life_map) 
+    initial_probability = .3
+
+    life_map = np.random.random((L,L))<=initial_probability
+
+    for iplot in range(n_iterations):
+        print(iplot)
+        PIL.Image.fromarray(life_map).save(f"pics/{iplot:06}.png")
+        neighbour_map = count_neighbours_roll(life_map)
+        life_map = neighbour_map==3 | (neighbour_map==2 & life_map) 
+
+def animate_pics():
+    subprocess.call('ffmpeg -y -r 20 -i pics/%06d.png -c:v mpeg4 -q:v 1 game_of_life.mp4', shell=True)    
+
+if __name__=="__main__":
+    game_of_life()
+    animate_pics()
